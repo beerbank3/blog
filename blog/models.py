c9c22ef233d566d0c1b5063e3a0bf654c2712cf0
beerbank3 <yg050140528@gmail.com>
2023-07-17 15:15:42 +0900

    0단계 완료

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model()
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.name
+    
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    writer = models.ForeignKey(User, on_delete=models.CASCADE)
+    upload_files = models.ImageField(upload_to="image")
+    categories = models.ManyToManyField(Category)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
+    content = models.TextField()
+    writer = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.content
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
-
+from django.views import View
+from .models import Post
 # Create your views here.
+### Post
+class Index(View):
+    
+    def get(self, request):
+        posts = Post.objects.prefetch_related('categories').all()
+        context = {
+            "posts": posts,
+            "title": "My Blog"
+        }
+        return render(request, 'blog/index.html', context)
+    
+
+class DetailView(View):
+    
+    def get(self, request, pk):
+        post = Post.objects.prefetch_related('categories').get(pk=pk)
+        
+        context = {
+            "title": "Blog Detail",
+            'post_id': pk,
+            'post_title': post.title,
+            'post_writer': post.writer,
+            'post_content': post.content,
+            'post_uploadfiles': post.upload_files,
+            'post_categories' : post.categories.all(),
+            'post_created_at': post.created_at.strftime('%Y-%m-%d'),
+        }
+        
+        return render(request, 'blog/post-detail.html', context)
